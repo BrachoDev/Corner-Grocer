254eca6f0a48666a7be99a024b9ba3213072d886
Carlos Bracho <brachocarlos152@gmail.com>
2024-12-13 09:58:45 -0500

    Initialized attributes and getters and setters

--- ItemTracker.h
+++ ItemTracker.h
@@ -4,7 +4,26 @@
 #include <iostream>
 using namespace std;
 
+class ItemTracker {
 
+private:
+    string ItemName;
+    int ItemFrequency;
+
+public:
+
+    // Constructor:
+    ItemTracker(string itemName, int itemFrequency);
+
+    // Getters:
+    string getItemName();
+    int getItemFrequency();
+
+    // Setters:
+    void setItemName(string itemName);
+    void setItemFrequency(int itemFrequency);
+
+};
 
 
 
